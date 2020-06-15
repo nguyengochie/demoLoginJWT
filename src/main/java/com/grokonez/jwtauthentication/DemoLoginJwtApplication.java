898b9e92a5e46c8ac0682bf7aa96622fcf80740d
hieu <nguyengoc.hie@gmail.com>
2020-06-15 18:02:05 +0700

    first push

---
+++ src/main/java/com/grokonez/jwtauthentication/DemoLoginJwtApplication.java
@@ -0,0 +1,13 @@
+package com.grokonez.jwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoginJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grokonez/jwtauthentication/DemoLoginJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grokonez.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoginJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
